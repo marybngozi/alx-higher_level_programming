a19a19af3464adbbfe1b180f281d20f38b343660
MaryBlessing <marybngozi@gmail.com>
2022-08-31 12:11:30 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv
     slen = len(argv) - 1
-    print(f"{slen} argument{'' if slen == 1 else 's'}\
-    {':' if slen >= 1 else '.'}")
+    print("{} argument{}{}"
+          .format(slen, '' if slen == 1 else 's', ':' if slen >= 1 else '.'))
     for i in range(1, len(argv)):
-        print(f"{i}: {argv[i]}")
+        print("{}: {}".format(i, argv[i]))
